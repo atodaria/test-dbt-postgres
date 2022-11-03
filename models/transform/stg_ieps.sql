0776e2652776f678af7647f1750046e3344e6b20
anant_gprki0y <anant.atodaria@outlook.com>
2022-11-04 00:31:32 +0530

    more

--- models/transform/stg_ieps.sql
+++ models/transform/stg_ieps.sql
@@ -1,6 +1,2 @@
-select 
-	sd.student_id,
-	sd.building,
-	sd.re_eval_due_date,
-	sd.current_iep_initiation_date
-from google_sheets.students_data as sd
+
+select * from {{ source('pgtestwithmodels_google_sheets', 'students_data') }}
