81129f330aeeac6ad9d093ed214849c32f65689a
anant_gprki0y <anant.atodaria@outlook.com>
2022-11-03 22:24:15 +0530

    connector for students

--- models/stg_students.sql
+++ models/stg_students.sql
@@ -1,11 +1,1 @@
-select 
-	sd.student_id,
-	sd.first_name, 
-	sd.last_name, 
-	sd.building, 
-	sd.school_district,
-	sd.grade,
-	sd.gender,
-	sd.language,
-	sd.dob
-from google_sheets.students_data as sd
+select * from {{ source('postgrestest_google_sheets', 'stg_students') }}
