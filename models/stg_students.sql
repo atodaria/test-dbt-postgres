d8348c5439952731a639104b5e1d789a534cc9a8
anant_gprki0y <anant.atodaria@outlook.com>
2022-11-03 22:34:04 +0530

    changes to models

--- models/stg_students.sql
+++ models/stg_students.sql
@@ -1,1 +1,11 @@
-select * from {{ source('postgrestest_google_sheets', 'stg_students') }}
+select 
+	sd.student_id,
+	sd.first_name, 
+	sd.last_name, 
+	sd.building, 
+	sd.school_district,
+	sd.grade,
+	sd.gender,
+	sd.language,
+	sd.dob
+from google_sheets.students_data as sd
