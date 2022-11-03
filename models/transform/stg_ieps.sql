52f96d962d49e25f29e982d419ed97b3aecffbc8
anant_gprki0y <anant.atodaria@outlook.com>
2022-11-04 00:45:52 +0530

    changes

--- models/transform/stg_ieps.sql
+++ models/transform/stg_ieps.sql
@@ -1,2 +1,16 @@
 
-select * from {{ source('pgtestwithmodels_google_sheets', 'students_data') }}
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select 
+	sd.student_id,
+	sd.building,
+	sd.re_eval_due_date,
+	sd.current_iep_initiation_date
+from google_sheets.students_data as sd
+
+)
+
+select *
+from source_data
